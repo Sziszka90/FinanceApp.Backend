7d0837de473943326a25ff79629cb179dad5a9bd
Szilard Ferencz <30410188+Sziszka90@users.noreply.github.com>
2025-07-15 07:07:54 +0200

    Update CustomWebApplicationFactory.cs

--- FinanceApp.Testing/Base/CustomWebApplicationFactory.cs
+++ FinanceApp.Testing/Base/CustomWebApplicationFactory.cs
@@ -45,5 +45,6 @@ public class CustomWebApplicationFactory<TProgram>
       services.RegisterJwtMock();
     });
     builder.UseEnvironment("Testing");
+services.Remove(typeof(IHostedService), typeof(ExchangeRateBackgroundJob));
   }
 }
