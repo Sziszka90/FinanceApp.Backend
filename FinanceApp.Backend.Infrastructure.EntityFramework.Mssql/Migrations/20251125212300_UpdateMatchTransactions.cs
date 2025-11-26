177cf48bf9ec0bd5be49a03b5ca7b24f7b014720
Szilard Ferencz <szilard.fer@gmail.com>
2025-11-26 21:41:58 +0100

    Fix linting

--- FinanceApp.Backend.Infrastructure.EntityFramework.Mssql/Migrations/20251125212300_UpdateMatchTransactions.cs
+++ FinanceApp.Backend.Infrastructure.EntityFramework.Mssql/Migrations/20251125212300_UpdateMatchTransactions.cs
@@ -1,52 +1,52 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace FinanceApp.Backend.Infrastructure.EntityFramework.Mssql.Migrations
 {
+  /// <inheritdoc />
+  public partial class UpdateMatchTransactions : Migration
+  {
     /// <inheritdoc />
-    public partial class UpdateMatchTransactions : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<DateTimeOffset>(
-                name: "Created",
-                table: "MatchTransaction",
-                type: "datetimeoffset",
-                nullable: false,
-                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+      migrationBuilder.AddColumn<DateTimeOffset>(
+          name: "Created",
+          table: "MatchTransaction",
+          type: "datetimeoffset",
+          nullable: false,
+          defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
 
-            migrationBuilder.AddColumn<Guid>(
-                name: "Id",
-                table: "MatchTransaction",
-                type: "uniqueidentifier",
-                nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+      migrationBuilder.AddColumn<Guid>(
+          name: "Id",
+          table: "MatchTransaction",
+          type: "uniqueidentifier",
+          nullable: false,
+          defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
 
-            migrationBuilder.AddColumn<DateTimeOffset>(
-                name: "Modified",
-                table: "MatchTransaction",
-                type: "datetimeoffset",
-                nullable: false,
-                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "Created",
-                table: "MatchTransaction");
-
-            migrationBuilder.DropColumn(
-                name: "Id",
-                table: "MatchTransaction");
-
-            migrationBuilder.DropColumn(
-                name: "Modified",
-                table: "MatchTransaction");
-        }
+      migrationBuilder.AddColumn<DateTimeOffset>(
+          name: "Modified",
+          table: "MatchTransaction",
+          type: "datetimeoffset",
+          nullable: false,
+          defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
     }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropColumn(
+          name: "Created",
+          table: "MatchTransaction");
+
+      migrationBuilder.DropColumn(
+          name: "Id",
+          table: "MatchTransaction");
+
+      migrationBuilder.DropColumn(
+          name: "Modified",
+          table: "MatchTransaction");
+    }
+  }
 }
